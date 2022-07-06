2e39335f56c2f2da67e70c858a45908ed38a27a1
angeldarco <angel92-eme@hotmail.com>
2022-07-06 16:13:26 +0000

    Almost Increasing Sequence solved

--- almostIncreasingSequence.js
+++ almostIncreasingSequence.js
@@ -1,27 +1,12 @@
 function solution(sequence) {
-    if (sequence.length >= 2 && sequence.length <= 10 ** 5) {
-      let x = 0;
-      let res = true;
-      if (x === 0 && sequence.length > 2) {
-        for (let i = 1; i < sequence.length; i++) {
-          if (x !== 0) break;
-          if (sequence[i - 1] > sequence[i]) {
-            sequence.splice(i, 1);
-            x++;
-          }
+    let data = 0;
+    for( let i = 1; i < sequence.length; i++ ){
+        if (sequence[i] <= sequence[i-1]){
+            data++;
+            if(data > 1) return false;
+            if(sequence[i] <= sequence[i-2] && sequence[i+1] <= sequence[i-1])
+            return false;
         }
-      }
-      for (let i = 0; i < sequence.length - 1; i++) {
-        if (sequence[i] === sequence[i + 1] && sequence.length === 2) {
-          return res;
-          break;
-        }
-        if (sequence[i] >= sequence[i + 1]) {
-          return !res;
-          break;
-        }
-      }
-      return res;
     }
-  }
-  
+    return true;
+  }  
