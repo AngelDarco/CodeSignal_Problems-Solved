5932d6be69329478da0712549fa6131690d3399a
Angel Darco <angel92-eme@hotmail.com>
2023-11-12 01:17:02 -0500

    :hammer: arrayMaximalAdjacentDifference solved

--- arrayMaximalAdjacentDifference.js
+++ arrayMaximalAdjacentDifference.js
@@ -1,11 +1,11 @@
 function solution(inputArray) {
-  const count = [];
+  const res = [];
   for (let i = 1; i < inputArray.length; i++) {
     if (inputArray[i - 1] > inputArray[i]) {
-      count.push(inputArray[i - 1] - inputArray[i]);
-    } else count.push(inputArray[i] - inputArray[i - 1]);
+      res.push(inputArray[i - 1] - inputArray[i]);
+    } else res.push(inputArray[i] - inputArray[i - 1]);
   }
-  count.sort((a, b) => a - b);
-  return count[count.length - 1];
+  res.sort((a, b) => a - b);
+  return res[res.length - 1];
 }
 module.exports = solution;
