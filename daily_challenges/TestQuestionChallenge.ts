ef72ed9f52d9b4d4b3b50dfea5a4568728550651
angeldarco <angel92-eme@hotmail.com>
2024-03-24 03:00:29 -0500

    :hammer: TestQuestionChallenge solved

---
+++ daily_challenges/TestQuestionChallenge.ts
@@ -0,0 +1,8 @@
+/* This is a test question challenge.
+For each happyMessage, return the expression followed by: HIU 2024. */
+
+function solution(happyMessage: string): string {
+  return happyMessage + " HIU 2024";
+}
+
+console.log(solution("Happy!!")); // Happy!! HIU 2024
