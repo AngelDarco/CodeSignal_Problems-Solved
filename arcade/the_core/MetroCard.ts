51b73b73e5f9c9c732c450ca7a5c205a3c30eb4a
angeldarco <angel92-eme@hotmail.com>
2024-03-28 12:46:44 -0500

    :hammer: MetroCard solved

---
+++ arcade/the_core/MetroCard.ts
@@ -0,0 +1,10 @@
+function solution(lastNumberOfDays: number) {
+  if (lastNumberOfDays === 31) {
+    return [28, 30, 31];
+  } else if (lastNumberOfDays === 30) {
+    return [31];
+  }
+  return [31];
+}
+
+console.log(solution(28)); // 31
